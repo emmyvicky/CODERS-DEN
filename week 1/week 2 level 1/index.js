777df398494ad0f1ea5f9e9a3ba50a208979ea22
emmyvicky <linkontobridget@gmail.com>
2022-10-15 23:06:31 +0100

    ain

---
+++ week 1/week 2 level 1/index.js
@@ -0,0 +1,26 @@
+let challenge =" '30 Days Of JavaScript";
+console.log(challenge);
+
+console.log(challenge.length)
+console.log(challenge.toUpperCase())
+console.log(challenge.toLowerCase())
+console.log(challenge.substring(4, 10))
+console.log(challenge.substr(4, 6))
+console.log(challenge.includes('Script'))
+console.log(challenge.split(','))
+console.log(challenge.replace('JavaScript', 'python'))
+console.log(challenge.charAt('15'))
+console.log(challenge.charCodeAt('J'))
+console.log(challenge.indexOf(4))
+console.log(challenge.lastIndexOf('Script'))
+console.log(challenge.indexOf('because')) 'You cannot end a sentence with because because because is a conjunction'
+console.log(challenge.search('because')) 'You cannot end a sentence with because because because is a conjunction'
+console.log(challenge.trim())
+console.log(challenge.startsWith())
+console.log(challenge.endsWith('30 Days Of JavaScript'))
+console.log(challenge.match('a'))
+console.log(challenge.concat())
+console.log(challenge.repeat(2))
+//Exercise: Level 2
+let quote = 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another
+console.log(quote);
