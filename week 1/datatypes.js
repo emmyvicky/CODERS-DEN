6dea2920a65c86aba07c6216abe30980b3865920
emmyvicky <linkontobridget@gmail.com>
2022-10-15 13:33:42 +0100

    week 1 assignment

---
+++ week 1/datatypes.js
@@ -0,0 +1,31 @@
+//checking data types
+let jsFile = 'we are just starting';
+console.log (typeof jsFile);//string
+let JavascriptIshard = true; 
+console.log (typeof javascriptIshard);//boollean
+let thisClass = null ;
+console.log (typeof thisClass);//null
+let mercy;
+console.log(mercy) // undefined, because it is not assigned to a value yet
+let godsWill = 30;
+console.log(typeof godsWill) // number
+//declearing variables without assigning values //
+let peace;
+let grace;
+let precious;
+let wisdom;
+//declearing variables and assigning values to them//
+let myHead = 'understand javascript';
+let thisJavascript = 'be easy for me';
+let ifNot = 'i will have sleepless nights';
+let godHelp = 'me' ;
+//declearing variables in multiple lines//
+let firstName = 'victoria';
+let lastName = 'Emmanuel';
+let maritalStatus = 'married';
+let country = 'Nigeria';
+let age = 28;
+//declearing variables in single lines//
+let herFirstname= 'victoria', herLastname = 'Emmanuel', herStatus = 'married',myCountry = 'Nigeria', herAge = 28;
+let myAge = 28;
+let yourAge = 30;
---
+++ week 1/variable.js
@@ -0,0 +1,7 @@
+// comments can make code readable//
+//Welcome to 30DaysOfJavaScript//
+/*comments can make code 
+readable, easy to reuse and informative*/
+let jsFile = "we are just starting"; //example of a string//
+let JavascriptIshard = true; //example of a Boolean//
+let thisClass = null; //example of a null//
